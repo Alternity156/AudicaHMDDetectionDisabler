504ab0f71c3c7a5fcc5d345a9dd069ec041044f5
Alternity156 <killercode_alternity_@hotmail.com>
2020-05-18 17:10:48 -0300

    Initial commit

---
+++ HMDDetectionDisabler/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+[assembly: AssemblyTitle(HMDDetectionDisabler.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(HMDDetectionDisabler.BuildInfo.Company)]
+[assembly: AssemblyProduct(HMDDetectionDisabler.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + HMDDetectionDisabler.BuildInfo.Author)]
+[assembly: AssemblyTrademark(HMDDetectionDisabler.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(HMDDetectionDisabler.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(HMDDetectionDisabler.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonModInfo(typeof(HMDDetectionDisabler.HMDDetectionDisabler), HMDDetectionDisabler.BuildInfo.Name, HMDDetectionDisabler.BuildInfo.Version, HMDDetectionDisabler.BuildInfo.Author, HMDDetectionDisabler.BuildInfo.DownloadLink)]
+
+
+// Create and Setup a MelonModGame to mark a Mod as Universal or Compatible with specific Games.
+// If no MelonModGameAttribute is found or any of the Values for any MelonModGame on the Mod is null or empty it will be assumed the Mod is Universal.
+// Values for MelonModGame can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonModGame(null, null)]
---
+++ HMDDetectionDisabler/Main.cs
@@ -0,0 +1,89 @@
+﻿using MelonLoader;
+using NET_SDK;
+using NET_SDK.Harmony;
+using System;
+
+namespace HMDDetectionDisabler
+{
+    public static class BuildInfo
+    {
+        public const string Name = "HMDDetectionDisabler"; // Name of the Mod.  (MUST BE SET)
+        public const string Author = "Alternity"; // Author of the Mod.  (Set as null if none)
+        public const string Company = null; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+    }
+
+    public class HMDDetectionDisabler : MelonMod
+    {
+        public static Patch PauseHandler_HMDMounted;
+        public static Patch PauseHandler_HMDUnmounted;
+
+        public override void OnApplicationStart()
+        {
+            Instance instance = Manager.CreateInstance("HMDDetectionDisabler");
+
+            PauseHandler_HMDMounted = instance.Patch(SDK.GetClass("PauseHandler").GetMethod("HMDMounted"), typeof(HMDDetectionDisabler).GetMethod("HMDMounted"));
+            PauseHandler_HMDUnmounted = instance.Patch(SDK.GetClass("PauseHandler").GetMethod("HMDUnmounted"), typeof(HMDDetectionDisabler).GetMethod("HMDUnmounted"));
+        }
+
+        public static unsafe void HMDMounted(IntPtr @this)
+        {
+            //PauseHandler_HMDMounted.InvokeOriginal(@this);
+            MelonModLogger.Log("HMDMounted");
+        }
+
+        public static unsafe void HMDUnmounted(IntPtr @this)
+        {
+            //PauseHandler_HMDMounted.InvokeOriginal(@this);
+            MelonModLogger.Log("HMDUnmounted");
+        }
+
+        /*
+        public override void OnLevelWasLoaded(int level)
+        {
+            MelonModLogger.Log("OnLevelWasLoaded: " + level.ToString());
+        }
+
+        public override void OnLevelWasInitialized(int level)
+        {
+            MelonModLogger.Log("OnLevelWasInitialized: " + level.ToString());
+        }
+
+        public override void OnUpdate()
+        {
+            MelonModLogger.Log("OnUpdate");
+        }
+
+        public override void OnFixedUpdate()
+        {
+            MelonModLogger.Log("OnFixedUpdate");
+        }
+
+        public override void OnLateUpdate()
+        {
+            MelonModLogger.Log("OnLateUpdate");
+        }
+
+        public override void OnGUI()
+        {
+            MelonModLogger.Log("OnGUI");
+        }
+
+        public override void OnApplicationQuit()
+        {
+            MelonModLogger.Log("OnApplicationQuit");
+        }
+
+        public override void OnModSettingsApplied()
+        {
+            MelonModLogger.Log("OnModSettingsApplied");
+        }
+
+        public override void VRChat_OnUiManagerInit() // Only works in VRChat
+        {
+            MelonModLogger.Log("VRChat_OnUiManagerInit");
+        }
+        */
+    }
+}
